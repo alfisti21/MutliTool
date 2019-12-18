794be47d260ed3436b711a7859af42c931a49a11
Angelos Ladopoulos <alfisti21@gmail.com>
2019-12-18 21:04:57 +0100

    Created functional Store Text fragment

--- app/src/main/java/com/ladopoulos/mutlitool/ui/storetext/StoretextFragment.java
+++ app/src/main/java/com/ladopoulos/mutlitool/ui/storetext/StoretextFragment.java
@@ -1,24 +1,38 @@
 package com.ladopoulos.mutlitool.ui.storetext;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
-
-import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
-
 import com.ladopoulos.mutlitool.R;
+
+import java.util.Objects;
 
 public class StoretextFragment extends Fragment {
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_storetext, container, false);
         final TextView textView = root.findViewById(R.id.text_storetext);
+        final EditText editText = root.findViewById(R.id.editText);
+        final Button button = root.findViewById(R.id.button);
+        final SharedPreferences myPrefs = Objects.requireNonNull(this.getActivity()).getSharedPreferences("prefs", Context.MODE_PRIVATE);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                SharedPreferences.Editor editor = myPrefs.edit();
+                editor.putString("STRING", editText.getText().toString());
+                editor.apply();
+                textView.setText(myPrefs.getString("STRING", null));
+            }
+        });
+        textView.setText(myPrefs.getString("STRING", null));
         return root;
     }
 }
