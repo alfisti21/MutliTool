baec21b5a1cb151b16ea82d79a659c5b89412a6b
Angelos Ladopoulos <alfisti21@gmail.com>
2019-12-19 20:08:41 +0100

    Web Fragment initial implementation

--- app/src/main/java/com/ladopoulos/mutlitool/MainActivity.java
+++ app/src/main/java/com/ladopoulos/mutlitool/MainActivity.java
@@ -2,23 +2,54 @@ package com.ladopoulos.mutlitool;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
+import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.tbruyelle.rxpermissions2.RxPermissions;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.params.BasicHttpParams;
+import org.apache.http.params.HttpConnectionParams;
+import org.apache.http.params.HttpParams;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
 import io.reactivex.functions.Consumer;
 
 public class MainActivity extends AppCompatActivity {
-
     @SuppressLint("CheckResult")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        try {
+            String s = new DownloadFilesTask().execute("https://www.eurail.com/en/get-inspired").get();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
         //RxPermission used for permissions. Saves code
         RxPermissions rxPermissions = new RxPermissions(this);
@@ -41,5 +72,55 @@ public class MainActivity extends AppCompatActivity {
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(navView, navController);
+    }
+
+    private class DownloadFilesTask extends AsyncTask<String, String, String> {
+        // these Strings / or String are / is the parameters of the task, that can be handed over via the excecute(params) method of AsyncTask
+        @RequiresApi(api = Build.VERSION_CODES.N)
+        protected String doInBackground(String... url) {
+            // constants
+            int timeoutSocket = 5000;
+            int timeoutConnection = 5000;
+
+            HttpParams httpParameters = new BasicHttpParams();
+            HttpConnectionParams.setConnectionTimeout(httpParameters, timeoutConnection);
+            HttpConnectionParams.setSoTimeout(httpParameters, timeoutSocket);
+            HttpClient client = new DefaultHttpClient(httpParameters);
+
+            HttpGet httpget = new HttpGet(url[0]);
+
+            try {
+                HttpResponse getResponse = client.execute(httpget);
+                final int statusCode = getResponse.getStatusLine().getStatusCode();
+
+                if(statusCode != HttpStatus.SC_OK) {
+                    Log.w("MyApp", "Download Error: " + statusCode + "| for URL: " + url);
+                    return null;
+                }
+
+                String line;
+                StringBuilder total = new StringBuilder();
+
+                HttpEntity getResponseEntity = getResponse.getEntity();
+
+                BufferedReader reader = new BufferedReader(new InputStreamReader(getResponseEntity.getContent()));
+
+                while((line = reader.readLine()) != null) {
+                    total.append(line);
+                }
+
+                line = total.toString();
+                Log.e("HTML", line);
+                return line;
+            } catch (Exception e) {
+                Log.w("MyApp", "Download Exception : " + e.toString());
+            }
+            return null;
+        }
+        // the onPostexecute method receives the return type of doInBackGround()
+        protected void onPostExecute(String result) {
+            // do something with the result, for example display the received Data in a ListView
+            // in this case, "result" would contain the "someLong" variable returned by doInBackground();
+        }
     }
 }
--- app/src/main/java/com/ladopoulos/mutlitool/ui/web/WebFragment.java
+++ app/src/main/java/com/ladopoulos/mutlitool/ui/web/WebFragment.java
@@ -1,9 +1,13 @@
 package com.ladopoulos.mutlitool.ui.web;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
@@ -14,22 +18,23 @@ import androidx.lifecycle.ViewModelProviders;
 
 import com.ladopoulos.mutlitool.R;
 
+import java.util.Objects;
+
 public class WebFragment extends Fragment {
 
-    private WebViewModel webViewModel;
-
-    public View onCreateView(@NonNull LayoutInflater inflater,
-                             ViewGroup container, Bundle savedInstanceState) {
-        webViewModel =
-                ViewModelProviders.of(this).get(WebViewModel.class);
+    @SuppressLint("SetJavaScriptEnabled")
+    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_web, container, false);
         final TextView textView = root.findViewById(R.id.text_web);
-        webViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
+        final WebView myWebView = root.findViewById(R.id.web_view);
+        myWebView.setInitialScale(1);
+        myWebView.getSettings().setDomStorageEnabled(true);
+        myWebView.getSettings().setLoadsImagesAutomatically(true);
+        myWebView.getSettings().setLoadWithOverviewMode(true);
+        myWebView.getSettings().setUseWideViewPort(true);
+        myWebView.getSettings().setBuiltInZoomControls(true);
+        myWebView.getSettings().setDisplayZoomControls(false);
+        myWebView.loadUrl("https://www.eurail.com/en/get-inspired");
         return root;
     }
 }
